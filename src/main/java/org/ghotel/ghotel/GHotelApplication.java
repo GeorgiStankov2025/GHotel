6c3838b1cd2005e313e1b6703767ef02c5a9fada
GeorgiStankov2025 <georgistankov05@gmail.com>
2026-08-07 15:59:20 +0300

    Initial commit

---
+++ src/main/java/org/ghotel/ghotel/GHotelApplication.java
@@ -0,0 +1,13 @@
+package org.ghotel.ghotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ghotel/ghotel/GHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ghotel.ghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
